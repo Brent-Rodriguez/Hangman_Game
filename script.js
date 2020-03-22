3e4f8304f6f2d02427df18a59023d1f3d433f5c1
Noo8 Cod3r <brent.s.rodriguez@gmail.com>
2020-03-22 00:02:24 -0500

    Add JS

---
+++ script.js
@@ -0,0 +1,6 @@
+const wordElement = document.getElementById('word')
+const wrongLettersElement = document.getElementById('wrong-letters')
+const playAgainBtn = document.getElementById('play-again')
+const popup = document.getElementById('popup-container')
+const notification = document.getElementById('notification-container')
+const finalMessage = document.getElementById('final-message')
